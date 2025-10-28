410f99c2684b9924a5fe86eab5f5407363029a2e
cyrizeh <89630353+nidrosoft@users.noreply.github.com>
2025-10-28 16:41:14 -0700

    fix: Update exports to match renamed step files
    - Export Step1Post from ./Step1Post
    - Export Step2Details from ./Step2Details
    - Export Step3Price from ./Step3Price
    - Remove 'as' aliases since files are already named correctly

--- src/components/sell/index.ts
+++ src/components/sell/index.ts
@@ -3,9 +3,9 @@
  */
 
 export { ProgressBar } from './ProgressBar';
-export { NewStep1Post as Step1Post } from './NewStep1Post';
-export { NewStep2Details as Step2Details } from './NewStep2Details';
-export { NewStep3Price as Step3Price } from './NewStep3Price';
+export { Step1Post } from './Step1Post';
+export { Step2Details } from './Step2Details';
+export { Step3Price } from './Step3Price';
 export { Step4Location } from './Step4Location';
 export { Step5Review } from './Step5Review';
 export { BusinessDetailsSection } from './BusinessDetailsSection';
